7344f6d46ddea475d9974f4675e77c8869de8c72
axi0712 <1255645340@qq.com>
2017-06-24 10:32:14 +0800

    闹闹帝 闹闹王闹闹星以及广场的页面布局展示

---
+++ app/src/main/java/com/example/citypass/cotroller/adapter/naonao/Net_Friend_NaoNao_Recycle_Adapter.java
@@ -0,0 +1,73 @@
+package com.example.citypass.cotroller.adapter.naonao;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * /**
+ * 项目名称: 血压卫士
+ * 类描述:
+ * 创建人: XI
+ * 创建时间: 2017/6/24 0024 10:21
+ * 修改人:
+ * 修改内容:
+ * 修改时间:
+ * #                                                   #
+ * #                       _oo0oo_                     #
+ * #                      o8888888o                    #
+ * #                      88" . "88                    #
+ * #                      (| -_- |)                    #
+ * #                      0\  =  /0                    #
+ * #                    ___/`---'\___                  #
+ * #                  .' \\|     |# '.                 #
+ * #                 / \\|||  :  |||# \                #
+ * #                / _||||| -:- |||||- \              #
+ * #               |   | \\\  -  #/ |   |              #
+ * #               | \_|  ''\---/''  |_/ |             #
+ * #               \  .-\__  '-'  ___/-. /             #
+ * #             ___'. .'  /--.--\  `. .'___           #
+ * #          ."" '<  `.___\_<|>_/___.' >' "".         #
+ * #         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       #
+ * #         \  \ `_.   \_ __\ /__ _/   .-` /  /       #
+ * #     =====`-.____`.___ \_____/___.-`___.-'=====    #
+ * #                       `=---='                     #
+ * #     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   #
+ * #                                                   #
+ * #               佛祖保佑         永无BUG              #
+ * #                                                   #
+ */
+
+
+public class Net_Friend_NaoNao_Recycle_Adapter extends RecyclerView.Adapter<Net_Friend_NaoNao_Recycle_Adapter.MyViewHolder> {
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+
+
+
+        return null;
+
+
+
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
